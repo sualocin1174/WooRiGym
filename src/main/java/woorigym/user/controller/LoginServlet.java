44d48ba06357b97f6dec8cfe0577f17fa381af11
junyebPark <wnsduq5694@gmail.com>
2021-10-11 17:38:46 +0900

    LoginServlet 오류 수정

--- src/main/java/woorigym/user/controller/LoginServlet.java
+++ src/main/java/woorigym/user/controller/LoginServlet.java
@@ -3,11 +3,13 @@ package woorigym.user.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import woorigym.user.model.service.UserService;
 
