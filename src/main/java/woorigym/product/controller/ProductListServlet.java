5ef7370019e0227d92ba73590093fc1f679b56c7
Seonhea <chsh9410@gmail.com>
2021-10-20 10:32:32 +0900

    파일업로드 코드수정 properties 포함

--- src/main/java/woorigym/product/controller/ProductListServlet.java
+++ src/main/java/woorigym/product/controller/ProductListServlet.java
@@ -25,30 +25,72 @@ public class ProductListServlet extends HttpServlet {
 		
 		String productNo = request.getParameter("productNo");
 		ArrayList<ProductTable> volist = new ProductService().readProductList();
-		
-		if(volist != null) {
-	         List<String> imagesFilePath= new ArrayList<String>();
-	         for(ProductTable vo : volist) {
-	            String infoUrl = vo.getProductInfoUrl();
-	            if (infoUrl !=null && !infoUrl.equals("")) {
-//	               File directory = new File("https://res.cloudinary.com/dgyk0qhqc/image/upload/v1634637136/woorigym/"+infoUrl);
-//	               File directory = new File("https://umarine.dothome.co.kr/images");
-//	               File directory = new File("https://cloudinary.com/console/c-7df1c4573ad22a712f4a8abe8c2eb9/media_library/folders/be4c1981d90944b7b5907e31e3b391e145");
-//	               File directory = new File("C:\\Users\\user1\\Pictures\\데이터베이스구현\\20210810");
-	               File directory = new File(getServletContext().getRealPath("product")+"/"+infoUrl);
-	               File[] flist = directory.listFiles();
-	               if(flist !=null) {
-	                  for (File file: flist) {
-	                     imagesFilePath.add("product"+"/"+infoUrl+"/"+file.getName());
-	                     if(file.isDirectory()) {
-	                        System.out.println("isDirectory?? 왜 체크하지?");
-	                     }
-	                  }
-	                  vo.setImagesFilePath(imagesFilePath);
-	               }//CARDIO-RN-0001-001
-	            }
-	         }
-	      }
+
+		if (volist != null) {
+			List<String> imagesFilePath = new ArrayList<String>();
+			
+			Properties prop = new Properties();
+			
+			/* 방법 1 dothome은 용량이 작음. 속도 느림 */
+			if(true) {
+			try {
+				String currentPath = ProductListServlet.class.getResource("./").getPath();
+				prop.load(new BufferedReader(new FileReader(currentPath + "cloudServer.properties")));
+				String server = prop.getProperty("server");
+				int port = Integer.parseInt(prop.getProperty("port"));
+				String user = prop.getProperty("user");
+				String pass = prop.getProperty("pass");
+
+				// FTP연결
+				FTPClient ftpClient = new FTPClient();
+				// connection 환경에서 UTF-8의 인코딩 타입을 사용한다.
+				ftpClient.setControlEncoding("UTF-8");
+				// ftp://localhost에 접속한다.
+				ftpClient.connect(server, 21);
+				// 접속을 확읺나다.
+				int resultCode = ftpClient.getReplyCode();
+				// 접속시 에러가 나오면 콘솔에 에러 메시지를 표시하고 프로그램을 종료한다.
+				if (!FTPReply.isPositiveCompletion(resultCode)) {
+					System.out.println("FTP server refused connection.!");
+					return;
+				} else {
+					// 파일 전송간 접속 딜레이 설정 (1ms 단위기 때문에 1000이면 1초)
+					ftpClient.setSoTimeout(1000);
+					// 로그인을 한다.
+					if (!ftpClient.login(user, pass)) {
+						// 로그인을 실패하면 프로그램을 종료한다.
+						System.out.println("Login Error!");
+						return;
+					}
+
+					// FTP연결 성공
+					
+					// 반복
+					for (ProductTable vo : volist) {
+						String infoUrl = vo.getProductInfoUrl();
+						if (infoUrl != null && !infoUrl.equals("")) {
+							infoUrl = infoUrl.substring(0, infoUrl.length() - 4);
+							// lists files and directories in the current working directory
+							FTPFile[] files = ftpClient.listFiles("/html/images" + "/" + infoUrl);
+							for (FTPFile file : files) {
+								String details = file.getName();
+								imagesFilePath
+										.add(prop.getProperty("url") + "/images" + "/" + infoUrl + "/" + details);
+								System.out.println("details:" + details);
+							}
+							System.out.println("imagesFilePath:" + imagesFilePath + ": "+ infoUrl);
+							vo.setImagesFilePath(imagesFilePath);
+						}
+					} // 반복 끝
+					
+					// FTP 끊기
+					ftpClient.logout();
+					ftpClient.disconnect();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			}
 		request.setAttribute("productvolist", volist);
 		String ViewPage = "/WEB-INF/productlist.jsp";
 		request.getRequestDispatcher(ViewPage).forward(request, response);
