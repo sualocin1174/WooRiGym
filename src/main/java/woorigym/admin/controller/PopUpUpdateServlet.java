ba33e62901350f181346263dd17da06bfa02b250
BGH0827 <pedov46@naver.com>
2021-10-25 17:08:36 +0900

    PopUpUpdateServlet 수정 // 수정이 완료되고나면 apupage로 이동되게 수정

--- src/main/java/woorigym/admin/controller/PopUpUpdateServlet.java
+++ src/main/java/woorigym/admin/controller/PopUpUpdateServlet.java
@@ -1,16 +1,12 @@
 package woorigym.admin.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import woorigym.admin.model.service.NoticeService;
 import woorigym.admin.model.vo.NoticeTable;
@@ -42,8 +38,8 @@ public class PopUpUpdateServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		Gson gson = new GsonBuilder().create();
+//		PrintWriter out = response.getWriter();
+//		Gson gson = new GsonBuilder().create();
 
 		String n_title = request.getParameter("notice_title");
 		String n_content = request.getParameter("notice_content");
@@ -54,17 +50,22 @@ public class PopUpUpdateServlet extends HttpServlet {
 		notice_noInt = Integer.parseInt(notice_no);
 		
 		NoticeTable noticeVo = new NoticeTable(n_title, n_content, n_date, notice_noInt);
+		
 		noticeVo.setN_title(n_title);
 		noticeVo.setN_content(n_content);
 		noticeVo.setNotice_no(notice_noInt);
 		System.out.println(noticeVo);
 		
 		int result = new NoticeService().updateNotice(noticeVo);
-		String jsonListVo = gson.toJson(result);
 		
-		out.print(jsonListVo);
-		out.flush();
-		out.close();
+		if(result == 1) {
+			request.setAttribute("msg", "공지사항이 등록되었습니다.");
+			response.sendRedirect("apupage");
+		}
+		else {
+			request.setAttribute("msh",  "공지사항이 등록되지 않았습니다.");
+			request.getRequestDispatcher("/WEB-INF/error.jsp").forward(request,  response);
+		}
 		
 //		if(result == 1) {
 //			
